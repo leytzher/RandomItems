c289e21e5e55f8ca2e5475628152c1b7507320d5
Family <family@MuroRochas-Mac-mini.local>
2015-07-16 18:20:20 +0800

    Initial Commit

---
+++ RandomItems/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Family on 7/16/15.
+//  Copyright (c) 2015 Leytzher Muro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
